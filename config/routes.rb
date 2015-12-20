d34b902ee68691aa21cba5a17e02399623df8db2
Joseph Leon <joseph.leon.9@gmail.com>
2015-12-20 16:42:04 -0500

    Update the markup on the home page and add resources to routes for debits and credits.

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  resources :debits
+  resources :credits
   root 'home#index'
 
   # Example of regular route:
