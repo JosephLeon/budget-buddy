8d89de62d2fb394cf3544d3ad1d5e5a60d7d9bd5
Joseph Leon <joseph.leon.9@gmail.com>
2015-12-21 17:53:19 -0500

    Add variables to calulate the sum of the monthly_value column in the credit and debit models.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,8 @@ class HomeController < ApplicationController
   def index
   	@credits = Credit.all
   	@debits = Debit.all
+
+  	@total_credits = Credit.sum(:monthly_value)
+  	@total_debits = Debit.sum(:monthly_value)
   end
 end
