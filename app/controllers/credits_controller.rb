6232e7f455af0c3f6cf647ecae25d71975ace5eb
Joseph Leon <joseph.leon.9@gmail.com>
2015-12-21 12:23:18 -0500

    Remove commented styles from application css, add destrpy method to credits controller, add delete link to credits area on home view, refacotr debots new view.

--- app/controllers/credits_controller.rb
+++ app/controllers/credits_controller.rb
@@ -10,6 +10,14 @@ class CreditsController < ApplicationController
 		redirect_to "/"
 	end
 
+	def destroy
+    @credit = Credit.find(params[:id])
+    @credit.destroy
+
+    flash[:success] = "Credit deleted"
+    redirect_to "/"
+  end
+
 	private
 	  def credit_params
 	    params.require(:credit).permit(:name, :amount, :interval)
