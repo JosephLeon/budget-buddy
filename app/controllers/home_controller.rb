4591bb5c4fbca580e2786e5eeebcc75fbec1161f
Joseph Leon <joseph.leon.9@gmail.com>
2015-12-21 18:15:47 -0500

    Add a base 30% taxes on credits.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
   	@credits = Credit.all
   	@debits = Debit.all
 
-  	@total_credits = Credit.sum(:monthly_value)
+  	@total_credits = (Credit.sum(:monthly_value)*70)/100
   	@total_debits = Debit.sum(:monthly_value)
   end
 end
